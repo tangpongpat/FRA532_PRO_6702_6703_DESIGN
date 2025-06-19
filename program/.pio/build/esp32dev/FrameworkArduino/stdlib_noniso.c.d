4d6f3d2183520fa030af1e3d7f5608c3fc158620
tangpongpat <65688901+tangpongpat@users.noreply.github.com>
2025-06-20 03:30:01 +0700

    ok

---
+++ program/.pio/build/esp32dev/FrameworkArduino/stdlib_noniso.c.d
@@ -0,0 +1,12 @@
+.pio/build/esp32dev/FrameworkArduino/stdlib_noniso.c.o: \
+ /Users/pongpat/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.c \
+ /Users/pongpat/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.h \
+ /Users/pongpat/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_system/include/esp_system.h \
+ /Users/pongpat/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /Users/pongpat/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /Users/pongpat/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
+ /Users/pongpat/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_bit_defs.h \
+ /Users/pongpat/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_idf_version.h \
+ /Users/pongpat/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_mac.h \
+ /Users/pongpat/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_chip_info.h \
+ /Users/pongpat/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_random.h
