87ef717398703df5c70b9c9341b7b68af99e9e02
tangpongpat <65688901+tangpongpat@users.noreply.github.com>
2025-06-20 04:47:30 +0700

    delete pio

--- program/.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
- /Users/pongpat/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- /Users/pongpat/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- /Users/pongpat/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- /Users/pongpat/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- program/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- /Users/pongpat/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /Users/pongpat/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
